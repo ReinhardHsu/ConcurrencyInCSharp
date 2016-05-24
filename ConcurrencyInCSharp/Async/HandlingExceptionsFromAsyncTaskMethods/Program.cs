4ff42d0d944545c5da274ffa1f66822d99c50f86
Reinhard <xuyafeng@vip.qq.com>
2016-05-24 20:14:51 +0800

    Async Exception

---
+++ ConcurrencyInCSharp/Async/HandlingExceptionsFromAsyncTaskMethods/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Nito.AsyncEx;
+
+namespace HandlingExceptionsFromAsyncTaskMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AsyncContext.Run(() =>
+                TestAsync()
+                );
+        }
+
+        static async Task ThrowExceptionAsync()
+        {
+            await Task.Delay(TimeSpan.FromSeconds(1));
+            throw new InvalidOperationException("test");
+        }
+
+        //static async Task TestAsync()
+        //{
+        //    try
+        //    {
+        //        await ThrowExceptionAsync();
+        //    }
+        //    catch(InvalidOperationException ex)
+        //    {
+
+        //    }
+        //}
+
+        static async Task TestAsync()
+        {
+            //抛出异常，并将其存储在Task中
+            Task task = ThrowExceptionAsync();
+            try
+            {
+                //Task对象被await调用，异常在这里再次被引发
+                await task;
+            }
+            catch (InvalidOperationException ex)
+            {
+                //异常在这里被捕获
+            }
+        }
+    }
+}
---
+++ ConcurrencyInCSharp/Async/ProcessingTasksAsTheyComplete/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Nito.AsyncEx;
+
+namespace ProcessingTasksAsTheyComplete
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            AsyncContext.Run(() =>
+                UseOrderByCompletionAsync()
+                );
+
+            Console.ReadKey();
+        }
+        
+        static async Task<int> DelayAndReturnAsync(int val)
+        {
+            await Task.Delay(TimeSpan.FromSeconds(val));
+            return val;
+        }
+
+        static async Task UseOrderByCompletionAsync()
+        {
+            Task<int> task1 = DelayAndReturnAsync(20);
+            Task<int> task2 = DelayAndReturnAsync(30);
+            Task<int> task3 = DelayAndReturnAsync(10);
+            var tasks = new[] { task1, task2, task3 }; 
+            
+            foreach(var task in tasks.OrderByCompletion())
+            {
+                var result=await task;
+                Console.WriteLine(result);
+            }
+        }
+
+        async Task ResumeOnContextAsync()
+        {
+            await Task.Delay(TimeSpan.FromSeconds(1));
+            // 这个方法在同一个上下文中恢复运行。
+        }
+        async Task ResumeWithoutContextAsync()
+        {
+            await Task.Delay(TimeSpan.FromSeconds(1)).ConfigureAwait(false);
+            // 这个方法在恢复运行时，会丢弃上下文。
+        }
+    }
+}
